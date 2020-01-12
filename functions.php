b7f469ca0bd47ae98c9c0c07a4d5c73fc560af08
Raju Jeelaga <rajujeelaga@gmail.com>
2020-01-12 15:28:15 +0530

    propermsetup for theme
    propermsetup for theme

--- functions.php
+++ functions.php
@@ -1,130 +1,515 @@
 <?php
 /**
- * Today News functions and definitions
-*/
+ * Twenty Twenty functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since 1.0.0
+ */
 
 /**
- * Today News only works in WordPress 4.7 or later.
+ * Table of Contents:
+ * Theme Support
+ * Required Files
+ * Register Styles
+ * Register Scripts
+ * Register Menus
+ * Custom Logo
+ * WP Body Open
+ * Register Sidebars
+ * Enqueue Block Editor Assets
+ * Enqueue Classic Editor Styles
+ * Block Editor Settings
  */
-if ( version_compare( $GLOBALS['wp_version'], '4.7', '<' ) ) {
-	require get_template_directory() . '/inc/back-compat.php';
-	return;
+
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which
+ * runs before the init hook. The init hook is too late for some features, such
+ * as indicating support for post thumbnails.
+ */
+function tt_theme_support() {
+
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
+
+	// Custom background color.
+	add_theme_support(
+		'custom-background',
+		array(
+			'default-color' => 'f5efe0',
+		)
+	);
+
+	// Set content-width.
+	global $content_width;
+	if ( ! isset( $content_width ) ) {
+		$content_width = 580;
+	}
+
+	/*
+	 * Enable support for Post Thumbnails on posts and pages.
+	 *
+	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+	 */
+	add_theme_support( 'post-thumbnails' );
+
+	// Set post thumbnail size.
+	set_post_thumbnail_size( 1200, 9999 );
+
+	// Add custom image size used in Cover Template.
+	add_image_size( 'tt-fullscreen', 1980, 9999 );
+
+	// Custom logo.
+	$logo_width  = 120;
+	$logo_height = 90;
+
+	// If the retina setting is active, double the recommended width and height.
+	if ( get_theme_mod( 'retina_logo', false ) ) {
+		$logo_width  = floor( $logo_width * 2 );
+		$logo_height = floor( $logo_height * 2 );
+	}
+
+	add_theme_support(
+		'custom-logo',
+		array(
+			'height'      => $logo_height,
+			'width'       => $logo_width,
+			'flex-height' => true,
+			'flex-width'  => true,
+		)
+	);
+
+	/*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
+
+	/*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+	add_theme_support(
+		'html5',
+		array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+			'script',
+			'style',
+		)
+	);
+
+	/*
+	 * Make theme available for translation.
+	 * Translations can be filed in the /languages/ directory.
+	 * If you're building a theme based on Twenty Twenty, use a find and replace
+	 * to change 'tt' to the name of your theme in all the template files.
+	 */
+	load_theme_textdomain( 'tt' );
+
+	// Add support for full and wide align images.
+	add_theme_support( 'align-wide' );
+
+	/*
+	 * Adds starter content to highlight the theme on fresh sites.
+	 * This is done conditionally to avoid loading the starter content on every
+	 * page load, as it is a one-off operation only needed once in the customizer.
+	 */
+	if ( is_customize_preview() ) {
+		require get_template_directory() . '/inc/starter-content.php';
+		add_theme_support( 'starter-content', tt_get_starter_content() );
+	}
+
+	// Add theme support for selective refresh for widgets.
+	add_theme_support( 'customize-selective-refresh-widgets' );
+
+	/*
+	 * Adds `async` and `defer` support for scripts registered or enqueued
+	 * by the theme.
+	 */
+	$loader = new tt_Script_Loader();
+	add_filter( 'script_loader_tag', array( $loader, 'filter_script_loader_tag' ), 10, 2 );
+
 }
 
-if ( ! function_exists( 'todaynews_setup' ) ) :
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * Note that this function is hooked into the after_setup_theme hook, which
-	 * runs before the init hook. The init hook is too late for some features, such
-	 * as indicating support for post thumbnails.
-	 */
-	function todaynews_setup() {
-		/*
-		 * Make theme available for translation.
-		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on Today News, use a find and replace
-		 * to change 'todaynews' to the name of your theme in all the template files.
-		 */
-		load_theme_textdomain( 'todaynews', get_template_directory() . '/languages' );
-
-		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
-
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
-		add_theme_support( 'title-tag' );
-
-		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
-		add_theme_support( 'post-thumbnails' );
-		set_post_thumbnail_size( 1568, 9999 );
-
-		// This theme uses wp_nav_menu() in two locations.
-		register_nav_menus(
-			array(
-				'menu-1' => __( 'Primary', 'todaynews' ),
-				'footer' => __( 'Footer Menu', 'todaynews' ),
-				'social' => __( 'Social Links Menu', 'todaynews' ),
-			)
-		);
-
-		/*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
-		add_theme_support(
-			'html5',
-			array(
-				'search-form',
-				'comment-form',
-				'comment-list',
-				'gallery',
-				'caption',
-			)
-		);
-
-		/**
-		 * Add support for core custom logo.
-		 *
-		 * @link https://codex.wordpress.org/Theme_Logo
-		 */
-		add_theme_support(
-			'custom-logo',
-			array(
-				'height'      => 190,
-				'width'       => 190,
-				'flex-width'  => false,
-				'flex-height' => false,
-			)
-		);
-
-		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
-
-		// Add support for Block Styles.
-		add_theme_support( 'wp-block-styles' );
-
-		// Add support for full and wide align images.
-		add_theme_support( 'align-wide' );
-
-		// Add support for editor styles.
-		add_theme_support( 'editor-styles' );
-
-		// Enqueue editor styles.
-		add_editor_style( 'style-editor.css' );
-
-	
-	}
-endif;
-add_action( 'after_setup_theme', 'todaynews_setup' );
+add_action( 'after_setup_theme', 'tt_theme_support' );
 
 /**
- * Register widget area.
+ * REQUIRED FILES
+ * Include required files.
+ */
+require get_template_directory() . '/inc/template-tags.php';
+
+// Handle SVG icons.
+require get_template_directory() . '/classes/class-tt-svg-icons.php';
+require get_template_directory() . '/inc/svg-icons.php';
+
+// Handle Customizer settings.
+require get_template_directory() . '/classes/class-tt-customize.php';
+
+// Require Separator Control class.
+require get_template_directory() . '/classes/class-tt-separator-control.php';
+
+// Custom comment walker.
+require get_template_directory() . '/classes/class-tt-walker-comment.php';
+
+// Custom page walker.
+require get_template_directory() . '/classes/class-tt-walker-page.php';
+
+// Custom script loader class.
+require get_template_directory() . '/classes/class-tt-script-loader.php';
+
+// Non-latin language handling.
+require get_template_directory() . '/classes/class-tt-non-latin-languages.php';
+
+// Custom CSS.
+require get_template_directory() . '/inc/custom-css.php';
+
+/**
+ * Register and Enqueue Styles.
+ */
+function tt_register_styles() {
+
+	$theme_version = wp_get_theme()->get( 'Version' );
+
+	wp_enqueue_style( 'tt-style', get_stylesheet_uri(), array(), $theme_version );
+	wp_style_add_data( 'tt-style', 'rtl', 'replace' );
+
+	// Add output of Customizer settings as inline style.
+	wp_add_inline_style( 'tt-style', tt_get_customizer_css( 'front-end' ) );
+
+	// Add print CSS.
+	wp_enqueue_style( 'tt-print-style', get_template_directory_uri() . '/print.css', null, $theme_version, 'print' );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'tt_register_styles' );
+
+/**
+ * Register and Enqueue Scripts.
+ */
+function tt_register_scripts() {
+
+	$theme_version = wp_get_theme()->get( 'Version' );
+
+	if ( ( ! is_admin() ) && is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+
+	wp_enqueue_script( 'tt-js', get_template_directory_uri() . '/assets/js/index.js', array(), $theme_version, false );
+	wp_script_add_data( 'tt-js', 'async', true );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'tt_register_scripts' );
+
+/**
+ * Fix skip link focus in IE11.
+ *
+ * This does not enqueue the script because it is tiny and because it is only for IE11,
+ * thus it does not warrant having an entire dedicated blocking script being loaded.
+ *
+ * @link https://git.io/vWdr2
+ */
+function tt_skip_link_focus_fix() {
+	// The following is minified via `terser --compress --mangle -- assets/js/skip-link-focus-fix.js`.
+	?>
+	<script>
+	/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
+	</script>
+	<?php
+}
+add_action( 'wp_print_footer_scripts', 'tt_skip_link_focus_fix' );
+
+/** Enqueue non-latin language styles
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function tt_non_latin_languages() {
+	$custom_css = tt_Non_Latin_Languages::get_non_latin_css( 'front-end' );
+
+	if ( $custom_css ) {
+		wp_add_inline_style( 'tt-style', $custom_css );
+	}
+}
+
+add_action( 'wp_enqueue_scripts', 'tt_non_latin_languages' );
+
+/**
+ * Register navigation menus uses wp_nav_menu in five places.
+ */
+function tt_menus() {
+
+	$locations = array(
+		'primary'  => __( 'Desktop Horizontal Menu', 'tt' ),
+		'expanded' => __( 'Desktop Expanded Menu', 'tt' ),
+		'mobile'   => __( 'Mobile Menu', 'tt' ),
+		'footer'   => __( 'Footer Menu', 'tt' ),
+		'social'   => __( 'Social Menu', 'tt' ),
+	);
+
+	register_nav_menus( $locations );
+}
+
+add_action( 'init', 'tt_menus' );
+
+/**
+ * Get the information about the logo.
+ *
+ * @param string $html The HTML output from get_custom_logo (core function).
+ *
+ * @return string $html
+ */
+function tt_get_custom_logo( $html ) {
+
+	$logo_id = get_theme_mod( 'custom_logo' );
+
+	if ( ! $logo_id ) {
+		return $html;
+	}
+
+	$logo = wp_get_attachment_image_src( $logo_id, 'full' );
+
+	if ( $logo ) {
+		// For clarity.
+		$logo_width  = esc_attr( $logo[1] );
+		$logo_height = esc_attr( $logo[2] );
+
+		// If the retina logo setting is active, reduce the width/height by half.
+		if ( get_theme_mod( 'retina_logo', false ) ) {
+			$logo_width  = floor( $logo_width / 2 );
+			$logo_height = floor( $logo_height / 2 );
+
+			$search = array(
+				'/width=\"\d+\"/iU',
+				'/height=\"\d+\"/iU',
+			);
+
+			$replace = array(
+				"width=\"{$logo_width}\"",
+				"height=\"{$logo_height}\"",
+			);
+
+			// Add a style attribute with the height, or append the height to the style attribute if the style attribute already exists.
+			if ( strpos( $html, ' style=' ) === false ) {
+				$search[]  = '/(src=)/';
+				$replace[] = "style=\"height: {$logo_height}px;\" src=";
+			} else {
+				$search[]  = '/(style="[^"]*)/';
+				$replace[] = "$1 height: {$logo_height}px;";
+			}
+
+			$html = preg_replace( $search, $replace, $html );
+
+		}
+	}
+
+	return $html;
+
+}
+
+add_filter( 'get_custom_logo', 'tt_get_custom_logo' );
+
+if ( ! function_exists( 'wp_body_open' ) ) {
+
+	/**
+	 * Shim for wp_body_open, ensuring backwards compatibility with versions of WordPress older than 5.2.
+	 */
+	function wp_body_open() {
+		do_action( 'wp_body_open' );
+	}
+}
+
+/**
+ * Include a skip to content link at the top of the page so that users can bypass the menu.
+ */
+function tt_skip_link() {
+	echo '<a class="skip-link screen-reader-text" href="#site-content">' . __( 'Skip to the content', 'tt' ) . '</a>';
+}
+
+add_action( 'wp_body_open', 'tt_skip_link', 5 );
+
+/**
+ * Register widget areas.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function todaynews_widgets_init() {
+function tt_sidebar_registration() {
 
+	// Arguments used in all register_sidebar() calls.
+	$shared_args = array(
+		'before_title'  => '<h2 class="widget-title subheading heading-size-3">',
+		'after_title'   => '</h2>',
+		'before_widget' => '<div class="widget %2$s"><div class="widget-content">',
+		'after_widget'  => '</div></div>',
+	);
+
+	// Footer #1.
 	register_sidebar(
-		array(
-			'name'          => __( 'Footer', 'todaynews' ),
-			'id'            => 'sidebar-1',
-			'description'   => __( 'Add widgets here to appear in your footer.', 'todaynews' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
+		array_merge(
+			$shared_args,
+			array(
+				'name'        => __( 'Footer #1', 'tt' ),
+				'id'          => 'sidebar-1',
+				'description' => __( 'Widgets in this area will be displayed in the first column in the footer.', 'tt' ),
+			)
+		)
+	);
+
+	// Footer #2.
+	register_sidebar(
+		array_merge(
+			$shared_args,
+			array(
+				'name'        => __( 'Footer #2', 'tt' ),
+				'id'          => 'sidebar-2',
+				'description' => __( 'Widgets in this area will be displayed in the second column in the footer.', 'tt' ),
+			)
 		)
 	);
 
 }
-add_action( 'widgets_init', 'todaynews_widgets_init' );
+
+add_action( 'widgets_init', 'tt_sidebar_registration' );
+
+/**
+ * Enqueue supplemental block editor styles.
+ */
+function tt_block_editor_styles() {
+
+	$css_dependencies = array();
+
+	// Enqueue the editor styles.
+	wp_enqueue_style( 'tt-block-editor-styles', get_theme_file_uri( '/assets/css/editor-style-block.css' ), $css_dependencies, wp_get_theme()->get( 'Version' ), 'all' );
+	wp_style_add_data( 'tt-block-editor-styles', 'rtl', 'replace' );
+
+	// Add inline style from the Customizer.
+	wp_add_inline_style( 'tt-block-editor-styles', tt_get_customizer_css( 'block-editor' ) );
+
+	// Add inline style for non-latin fonts.
+	wp_add_inline_style( 'tt-block-editor-styles', tt_Non_Latin_Languages::get_non_latin_css( 'block-editor' ) );
+
+	// Enqueue the editor script.
+	wp_enqueue_script( 'tt-block-editor-script', get_theme_file_uri( '/assets/js/editor-script-block.js' ), array( 'wp-blocks', 'wp-dom' ), wp_get_theme()->get( 'Version' ), true );
+}
+
+add_action( 'enqueue_block_editor_assets', 'tt_block_editor_styles', 1, 1 );
+
+/**
+ * Enqueue classic editor styles.
+ */
+function tt_classic_editor_styles() {
+
+	$classic_editor_styles = array(
+		'/assets/css/editor-style-classic.css',
+	);
+
+	add_editor_style( $classic_editor_styles );
+
+}
+
+add_action( 'init', 'tt_classic_editor_styles' );
+
+/**
+ * Output Customizer settings in the classic editor.
+ * Adds styles to the head of the TinyMCE iframe. Kudos to @Otto42 for the original solution.
+ *
+ * @param array $mce_init TinyMCE styles.
+ *
+ * @return array $mce_init TinyMCE styles.
+ */
+function tt_add_classic_editor_customizer_styles( $mce_init ) {
+
+	$styles = tt_get_customizer_css( 'classic-editor' );
+
+	if ( ! isset( $mce_init['content_style'] ) ) {
+		$mce_init['content_style'] = $styles . ' ';
+	} else {
+		$mce_init['content_style'] .= ' ' . $styles . ' ';
+	}
+
+	return $mce_init;
+
+}
+
+add_filter( 'tiny_mce_before_init', 'tt_add_classic_editor_customizer_styles' );
+
+
+
+
+/**
+ * Overwrite default more tag with styling and screen reader markup.
+ *
+ * @param string $html The default output HTML for the more tag.
+ *
+ * @return string $html
+ */
+function tt_read_more_tag( $html ) {
+	return preg_replace( '/<a(.*)>(.*)<\/a>/iU', sprintf( '<div class="read-more-button-wrap"><a$1><span class="faux-button">$2</span> <span class="screen-reader-text">"%1$s"</span></a></div>', get_the_title( get_the_ID() ) ), $html );
+}
+
+add_filter( 'the_content_more_link', 'tt_read_more_tag' );
+
+/**
+ * Enqueues scripts for customizer controls & settings.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function tt_customize_controls_enqueue_scripts() {
+	$theme_version = wp_get_theme()->get( 'Version' );
+
+	// Add main customizer js file.
+	wp_enqueue_script( 'tt-customize', get_template_directory_uri() . '/assets/js/customize.js', array( 'jquery' ), $theme_version, false );
+
+	// Add script for color calculations.
+	wp_enqueue_script( 'tt-color-calculations', get_template_directory_uri() . '/assets/js/color-calculations.js', array( 'wp-color-picker' ), $theme_version, false );
+
+	// Add script for controls.
+	wp_enqueue_script( 'tt-customize-controls', get_template_directory_uri() . '/assets/js/customize-controls.js', array( 'tt-color-calculations', 'customize-controls', 'underscore', 'jquery' ), $theme_version, false );
+	wp_localize_script( 'tt-customize-controls', 'ttBgColors', tt_get_customizer_color_vars() );
+}
+
+add_action( 'customize_controls_enqueue_scripts', 'tt_customize_controls_enqueue_scripts' );
+
+/**
+ * Enqueue scripts for the customizer preview.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function tt_customize_preview_init() {
+	$theme_version = wp_get_theme()->get( 'Version' );
+
+	wp_enqueue_script( 'tt-customize-preview', get_theme_file_uri( '/assets/js/customize-preview.js' ), array( 'customize-preview', 'customize-selective-refresh', 'jquery' ), $theme_version, true );
+	wp_localize_script( 'tt-customize-preview', 'ttBgColors', tt_get_customizer_color_vars() );
+	wp_localize_script( 'tt-customize-preview', 'ttPreviewEls', tt_get_elements_array() );
+
+	wp_add_inline_script(
+		'tt-customize-preview',
+		sprintf(
+			'wp.customize.selectiveRefresh.partialConstructor[ %1$s ].prototype.attrs = %2$s;',
+			wp_json_encode( 'cover_opacity' ),
+			wp_json_encode( tt_customize_opacity_range() )
+		)
+	);
+}
+
+add_action( 'customize_preview_init', 'tt_customize_preview_init' );
+
+
+
 
